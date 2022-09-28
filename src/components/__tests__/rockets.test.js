a9d7889c7249428b086326466cec21d0a8e30ef0
Matias Aguirre <aguirremati09@gmail.com>
2022-09-28 16:23:00 -0300

    Fixed Styles

--- src/components/__tests__/rockets.test.js
+++ src/components/__tests__/rockets.test.js
@@ -1,7 +1,7 @@
 import rocketsReducer from '../../redux/Rockets/rockets';
 
 describe('Testing for add reservation action in rocketsReducer ', () => {
-    test('when the action is add reservation, and the id provided is wrong, return same state', () => {
+  test('when the action is add reservation, and the id provided is wrong, return same state', () => {
     // arrange
     const RESERVATION = 'space-travelers/rockets/ADD_ROCKET_RESERVATION';
     const action = { type: RESERVATION, id: '2' };
@@ -21,7 +21,7 @@ describe('Testing for add reservation action in rocketsReducer ', () => {
 });
 
 describe('Testing for remove reservation action in rocketsReducer ', () => {
-    test('when the action is remove reservation, and the id provided is wrong, return same state', () => {
+  test('when the action is remove reservation, and the id provided is wrong, return same state', () => {
     // arrange
     const CANCEL_RESERVATION = 'space-travelers/rockets/REMOVE__ROCKET_RESERVATION';
     const action = { type: CANCEL_RESERVATION, id: '2' };
@@ -40,7 +40,7 @@ describe('Testing for remove reservation action in rocketsReducer ', () => {
   });
 });
 
-  describe('Testing for wrong action in rocketsReducer ', () => {
+describe('Testing for wrong action in rocketsReducer ', () => {
   test('when the action is wrong, we return the same state with no changes', () => {
     // arrange
     const state = [
